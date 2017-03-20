c5795eb27264f7edd851742f0b609b961fc353f6
Shipra Gupta <shipra.vgupta@gmail.com>
2017-03-19 22:54:46 -0400

    Add files via upload
    Computing the inverse of a square matrix using R Programming.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# Here, makeCacheMatrix contains a function which does 4 tasks:
+#Firstly, set the value of the matrix
+#Secondly, get the value of the matrix
+#Thirdly, set the value of inverse of matrix
+#Fourthly, get the value of inverse of matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set=set, 
+       get=get, 
+       setinverse=setinverse, 
+       getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+# The following function first checks if the inverse is already done. 
+# If it is, it fetches the result and skips calculations. 
+# If it's not,it calculates the inverse, sets the value in cache through setinverse function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data!")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setinverse(inv)
+  inv
 }
+# Test for Solution
+#> i <- matrix(c(7,8,9,10),2,2)
+#> j <- makeCacheMatrix(i)
+#> cacheSolve(j)
+# [,1] [,2]
+# [1,]   -5  4.5
+# [2,]    4 -3.5
+#> cacheSolve(j)
+#getting cached data!
+# [,1] [,2]
+# [1,]   -5  4.5
+# [2,]    4 -3.5
